8339cbd3e9c93bfad3054e30e63f6ddfc0498eea
Artem A <shepardcmndr@gmail.com>
2023-11-15 18:10:28 +0400

    Code prepared for docker

--- src/main/java/ru/skillbox/adminpanel/SocialNetAdminPanel.java
+++ src/main/java/ru/skillbox/adminpanel/SocialNetAdminPanel.java
@@ -10,7 +10,6 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @ConfigurationPropertiesScan
-//@EnableScheduling
 @Configuration
 @EnableAspectJAutoProxy
 public class SocialNetAdminPanel {
